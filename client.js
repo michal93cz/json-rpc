662e103bbd67230f5ba61ebc653112caadc00a43
Czupryniak, Michal <michal.czupryniak@pearson.com>
2017-11-10 21:49:52 +0100

    Completed project

--- client.js
+++ client.js
@@ -4,3 +4,26 @@ const jayson = require('jayson');
 const client = jayson.client.http({
     port: 3000
 });
+
+// invoke "sayHello"
+client.request('sayHello', {}, (err, response) => {
+    if(err) throw err;
+    console.log(response.result);
+});
+
+// invoke "add"
+client.request('add', [3, 3], (err, response) => {
+    if(err) throw err;
+    console.log(response.result);
+});
+
+// invoke "getUser"
+client.request('getUser', { name: 'Anna' }, (err, response) => {
+    if(err) throw err;
+    console.log(response.result);
+});
+
+// invoke "notFound"
+client.request('notFound', {}, (err, response) => {
+    if(err) console.error(err);
+});
--- server.js
+++ server.js
@@ -1,8 +1,34 @@
 const jayson = require('jayson');
 
+const usersData = [
+    {
+        name: 'Kirsten',
+        email: 'kirsten.greenholt@corkeryfisher.info'
+    },
+    {
+        name: 'Anna',
+        email: 'anna.white@gmail.com'
+    },
+    {
+        name: 'Christophe',
+        email: 'christophe.johnson@corkeryfisher.com'
+    },
+    {
+        name: 'Bartell',
+        email: 'bartell.wood@gmail.com'
+    },
+    {
+        name: 'Anna',
+        email: 'anna.webber@corkeryfisher.com'
+    }
+];
+
 // create a server
 const server = jayson.server({
-    // object with methods
+    sayHello: (args, callback) => callback(null, 'Hello'),
+    add: (args, callback) => callback(null, args[0] + args[1]),
+    getUser: (args, callback) => callback(null, usersData.filter((object) => object.name === args.name || object.email === args.email)),
+    notFound: (args, callback) => callback({code: 404, message: 'Not Found'})
 });
 
 server.http().listen(3000);
